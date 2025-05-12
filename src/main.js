c35d4232fcb4d056d4312fa704867cc91db173df
Bryan Buchs <bryan@kapowinc.com>
2025-05-12 10:03:18 -0500

    Remove photoswipe-dynamic-caption-plugin and related code for cleaner implementation

--- src/main.js
+++ src/main.js
@@ -32,14 +32,11 @@ stickyHeaders.forEach(el => {
 import PhotoSwipe from 'photoswipe'
 import PhotoSwipeLightbox from 'photoswipe/lightbox'
 import ObjectPosition from '@vovayatsyuk/photoswipe-object-position'
-// import PhotoSwipeDynamicCaption from 'photoswipe-dynamic-caption-plugin'
 import 'photoswipe/style.css'
-// import 'photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.css'
 
 const lightbox = new PhotoSwipeLightbox({
   gallery: '.screenshots',
   children: 'a:has(img)',
-  // showHideAnimationType: 'fade',
   showAnimationDuration: 400,
   hideAnimationDuration: 250,
   easing: 'cubic-bezier(0.77, 0, 0.175, 1)',
@@ -48,16 +45,6 @@ const lightbox = new PhotoSwipeLightbox({
   padding: { top: 20, bottom: 20, left: 20, right: 20 },
   pswpModule: PhotoSwipe
 })
-
-// new PhotoSwipeDynamicCaption(lightbox, {
-//   type: 'below',
-//   captionContent: slide => {
-//     return slide.data.element
-//       .querySelector('img')
-//       .closest('figure')
-//       .querySelector('figcaption').innerHTML
-//   }
-// })
 
 new ObjectPosition(lightbox)
 
